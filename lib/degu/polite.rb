c54b8fbd485dbde520ef88bccc3639d21bd09ee3
Florian Frank <flori@ping.de>
2015-05-06 15:54:10 +0200

    Add deprecation warning for older requires

---
+++ lib/degu/polite.rb
@@ -0,0 +1,2 @@
+require 'degu'
+warn "Requiring 'degu/polite' is deprecated, require 'degu' directly now."
---
+++ lib/degu/rude.rb
@@ -0,0 +1,2 @@
+require 'degu'
+warn "Requiring 'degu/rude' is deprecated, require 'degu' directly now."
